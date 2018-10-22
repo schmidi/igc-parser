46f3349bfa44b5d2417ca1e026e8874f8d5e071a
Tobias Schmid <schmid.tobias@bluewin.ch>
2018-10-22 22:53:01 +0200

    improve library, add tests

---
+++ test/IGCParserTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+use IGCParser\IGCParser;
+use PHPUnit\Framework\TestCase;
+
+
+final class IGCParserTest extends TestCase {
+
+
+    public function testLoadFromFile() {
+
+        $igcFile = IGCParser::fromFile(__DIR__ . "/example_files/8ALTobi1.igc");
+
+        $this->assertNotNull($igcFile);
+        $this->assertEquals("Tobias Schmid", $igcFile->pilot);
+        $this->assertEquals("Ozone Jomo", $igcFile->glider_type);
+        //$this->assertEquals("SKYTRAXX", $igcFile->manufacturer);
+
+        var_dump($igcFile);
+
+        try {
+            var_dump($igcFile->getTrackPoints());
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/IGCRecordTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+use IGCParser\IGCParser;
+use PHPUnit\Framework\TestCase;
+
+
+final class IGCRecordTest extends TestCase {
+
+
+    public function testARecord() {
+
+        $aRecord = new \IGCParser\Model\IGC_A_Record("AXSX001 SKYTRAXX V1.89 SN:2956932040");
+
+        $this->assertNotNull($aRecord);
+        $this->assertEquals("AXSX001 SKYTRAXX V1.89 SN:2956932040", $aRecord->raw);
+        $this->assertEquals("A", $aRecord->type);
+        $this->assertEquals("X", $aRecord->manufacturer);
+        $this->assertEquals("SX001", $aRecord->unique_id);
+        $this->assertEquals(" SKYTRAXX V1.89 SN:2956932040", $aRecord->id_extension);
+
+    }
+
+    public function testHRecord() {
+
+        $inputString = "HOPLTPILOT: Tobias Schmid";
+        $hRecord = new \IGCParser\Model\IGC_H_Record($inputString);
+
+        $this->assertEquals("H", $hRecord->type);
+        $this->assertEquals($inputString, $hRecord->raw);
+        $this->assertEquals("O", $hRecord->source);
+        $this->assertEquals("PLT", $hRecord->tlc);
+        $this->assertEquals("PILOT", $hRecord->key);
+        $this->assertEquals(" Tobias Schmid", $hRecord->value);
+
+
+        $inputString = "HOGTYGLIDERTYPE: Ozone Jomo";
+        $hRecord = new \IGCParser\Model\IGC_H_Record($inputString);
+
+        $this->assertEquals("H", $hRecord->type);
+        $this->assertEquals($inputString, $hRecord->raw);
+        $this->assertEquals("O", $hRecord->source);
+        $this->assertEquals("GTY", $hRecord->tlc);
+        $this->assertEquals("GLIDERTYPE", $hRecord->key);
+        $this->assertEquals(" Ozone Jomo", $hRecord->value);
+
+        // TODO: extend further types
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
